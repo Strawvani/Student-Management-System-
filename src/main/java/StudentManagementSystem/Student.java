a6a227b44cd163afb32c2788b74ac6bc2ef08865
Jordan Gonzales <jordanmenddozagonzales31@gmail.com>
2026-04-02 23:40:36 +0800

    Remove Student Class Arraylist of courseEnrolled, Remove enroll();

--- src/main/java/StudentManagementSystem/Student.java
+++ src/main/java/StudentManagementSystem/Student.java
@@ -6,15 +6,9 @@ public class Student extends Person{
 
     private int studentID;
 
-    ArrayList<Course> enrolledCourses = new ArrayList<>();
-
     public Student (String name, int ID){
        super(name);
         this.studentID = ID;
-    }
-
-    public void enroll(Course course){
-        enrolledCourses.add(course);
     }
 
     public String getRole(){
